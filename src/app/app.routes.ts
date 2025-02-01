4c41466eaee59b1961f8438593063165bbf76a17
Wiper <mactolik1993@gmail.com>
2025-02-01 15:00:08 +0200

    create component and add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TableComponent } from './components/table/table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: TableComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
