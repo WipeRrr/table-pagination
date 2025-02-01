03dca5c277d725ddbcae91ac86d840cdf349a356
Wiper <mactolik1993@gmail.com>
2025-02-01 16:03:42 +0200

    add table and pagination

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,6 +16,7 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        
       },
     }),
   ],
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../service/productservice';
+import { Product } from '../../interface/product';
+
+
+interface Column {
+  field: string;
+  header: string;
+}
+
 
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [TableModule, CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
+  providers: [ProductService],
 })
-export class TableComponent {}
+export class TableComponent {
+  products!: Product[];
+
+  cols!: Column[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().then((data) => {
+      this.products = data;
+    });
+
+    this.cols = [
+      { field: 'code', header: 'Code' },
+      { field: 'name', header: 'Name' },
+      { field: 'category', header: 'Category' },
+      { field: 'quantity', header: 'Quantity' },
+      { field: 'rating', header: 'Rating' },
+    ];
+  }
+}
